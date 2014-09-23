488bde8eed7d1f88b828302193206a01bd0f8137
reiz <robert.reiz.81@gmail.com>
2014-09-23 16:15:07 +0200

    little update

--- job_scheduler.sh
+++ job_scheduler.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/bin/bash -l -c 'cd /versioneye-tasks; /usr/local/bin/bundle exec rake versioneye:scheduler --silent'
+/bin/bash -l -c 'cd /versioneye-tasks; /usr/local/bin/bundle exec rake versioneye:scheduler'
