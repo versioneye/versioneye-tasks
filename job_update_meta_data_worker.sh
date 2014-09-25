b1ea4bb649ad86cc673241db960aedc2d09bedb4
reiz <robert.reiz.81@gmail.com>
2014-09-25 14:23:53 +0200

    Update scripts

--- job_update_meta_data_worker.sh
+++ job_update_meta_data_worker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/bin/bash -l -c 'cd /versioneye-tasks; /usr/local/bin/bundle exec rake versioneye:update_meta_data_worker &'
+/bin/bash -l -c 'cd /versioneye-tasks; /usr/local/bin/bundle exec rake versioneye:update_meta_data_worker'
