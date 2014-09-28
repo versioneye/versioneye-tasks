b95876827c5db478574b01a684a7f7bf5cc59ec4
reiz <robert.reiz.81@gmail.com>
2014-09-28 20:20:27 +0200

    Update job_rabbit_all.sh

--- job_rabbit_all.sh
+++ job_rabbit_all.sh
@@ -2,9 +2,7 @@
 
 /bin/bash -l -c 'cd /versioneye-tasks; /usr/local/bin/bundle exec rake versioneye:github_repos_import_worker &'
 /bin/bash -l -c 'cd /versioneye-tasks; /usr/local/bin/bundle exec rake versioneye:github_repos_import_worker &'
-/bin/bash -l -c 'cd /versioneye-tasks; /usr/local/bin/bundle exec rake versioneye:github_repos_import_worker &'
 
-/bin/bash -l -c 'cd /versioneye-tasks; /usr/local/bin/bundle exec rake versioneye:github_repo_import_worker &'
 /bin/bash -l -c 'cd /versioneye-tasks; /usr/local/bin/bundle exec rake versioneye:github_repo_import_worker &'
 /bin/bash -l -c 'cd /versioneye-tasks; /usr/local/bin/bundle exec rake versioneye:github_repo_import_worker &'
 
