f9b65e55fd9d00b573f1e2b97157ae01bd0306a7
reiz <robert.reiz.81@gmail.com>
2014-11-17 19:11:13 +0100

    Update workers

--- job_project_update_worker.sh
+++ job_project_update_worker.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+/bin/bash -l -c 'cd /versioneye-tasks; /usr/local/bin/bundle exec rake versioneye:project_update_worker --silent &'
+/bin/bash -l -c 'cd /versioneye-tasks; /usr/local/bin/bundle exec rake versioneye:project_update_worker --silent &'
 /bin/bash -l -c 'cd /versioneye-tasks; /usr/local/bin/bundle exec rake versioneye:project_update_worker --silent'
