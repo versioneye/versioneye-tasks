2b07206bd30ce1fe9de98c82c5009aa6192ff2ca
reiz <robert.reiz.81@gmail.com>
2015-01-26 09:14:34 +0100

    Update core

--- job_rabbit_all.sh
+++ job_rabbit_all.sh
@@ -16,7 +16,6 @@
 /bin/bash -l -c 'cd /versioneye-tasks; /usr/local/bin/bundle exec rake versioneye:project_update_worker &'
 /bin/bash -l -c 'cd /versioneye-tasks; /usr/local/bin/bundle exec rake versioneye:project_update_worker &'
 /bin/bash -l -c 'cd /versioneye-tasks; /usr/local/bin/bundle exec rake versioneye:update_meta_data_worker &'
-/bin/bash -l -c 'cd /versioneye-tasks; /usr/local/bin/bundle exec rake versioneye:update_dependencies_worker &'
 /bin/bash -l -c 'cd /versioneye-tasks; /usr/local/bin/bundle exec rake versioneye:update_index_worker &'
 /bin/bash -l -c 'cd /versioneye-tasks; /usr/local/bin/bundle exec rake versioneye:send_notification_emails_worker &'
 /bin/bash -l -c 'cd /versioneye-tasks; /usr/local/bin/bundle exec rake versioneye:common_worker &'
