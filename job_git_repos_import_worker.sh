0a152c5ae5f96e01b31b500e56f1949cde1d9eb1
reiz <robert.reiz.81@gmail.com>
2015-11-15 17:13:46 +0100

    Remove unused files.

--- job_git_repos_import_worker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-/bin/bash -l -c 'cd /versioneye-tasks; /usr/local/bin/bundle exec rake versioneye:git_repos_import_worker &'
-/bin/bash -l -c 'cd /versioneye-tasks; /usr/local/bin/bundle exec rake versioneye:git_repos_import_worker &'
-/bin/bash -l -c 'cd /versioneye-tasks; /usr/local/bin/bundle exec rake versioneye:git_repos_import_worker &'
-/bin/bash -l -c 'cd /versioneye-tasks; /usr/local/bin/bundle exec rake versioneye:git_repos_import_worker'
