1e4be2f7e1523628869c49d9191d306ca05dea2f
reiz <robert.reiz.81@gmail.com>
2017-07-31 11:47:11 +0200

    Add comment

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Mount your site certificate into
+# /usr/local/share/ca-certificates as *.crt file
+# if you want to make self signed certificates
+# available
+
 sudo update-ca-certificates;
 
 /usr/bin/supervisord -c /etc/supervisord.conf
